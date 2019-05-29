aefced87971a388f55e4b58a82563a6868b35ee5
chase <1554658086@qq.com>
2019-05-29 09:26:18 +0800

    update

--- mean-docker/express-server/public/js/services/todos.js
+++ mean-docker/express-server/public/js/services/todos.js
@@ -7,8 +7,8 @@ angular.module('todoService', [])
 			get : function() {
 				return $http.get('/api/todos');
 			},
-			create : function(todoData) {
-				return $http.post('/api/todos', todoData);
+			create : function(user) {
+				return $http.post('/api/todos', user);
 			},
 			delete : function(id) {
 				return $http.delete('/api/todos/' + id);
