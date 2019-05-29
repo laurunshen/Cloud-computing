555f7ae98a9484d80d9447b9204728e26a83b5f7
chase <1554658086@qq.com>
2019-05-29 11:19:16 +0800

    updated

--- mean-docker/express-server/public/js/controllers/main.js
+++ mean-docker/express-server/public/js/controllers/main.js
@@ -51,7 +51,7 @@ angular.module('todoController', [])
 					// if successful creation, call our get function to get all the new todos
 					.success(function(data) {
 						$scope.loading = false;
-						$scope.formData = {}; // clear the form so our user is ready to enter another
+						$scope.user = {}; // clear the form so our user is ready to enter another
 				//		$scope.todos = data; // assign our new list of todos
 					});
 
