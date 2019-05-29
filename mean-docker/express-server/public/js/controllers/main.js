4da2809421b952449ef75d44e0020d318eda61e0
chase <1554658086@qq.com>
2019-05-29 09:36:16 +0800

    update

--- mean-docker/express-server/public/js/controllers/main.js
+++ mean-docker/express-server/public/js/controllers/main.js
@@ -20,46 +20,25 @@ angular.module('todoController', [])
 
 			// validate the formData to make sure that something is there
 			// if form is empty, nothing will happen
-			if ($scope.formData.text != undefined) {
+			if ($scope.user.account != undefined) {
 				$scope.loading = true;
 
 				// call the create function from our service (returns a promise object)
-				Todos.create($scope.formData)
+				Todos.create($scope.user)
 
 					// if successful creation, call our get function to get all the new todos
 					.success(function(data) {
 						$scope.loading = false;
 						$scope.formData = {}; // clear the form so our user is ready to enter another
-						$scope.todos = data; // assign our new list of todos
+				//		$scope.todos = data; // assign our new list of todos
 					});
-
-
-
 
 
 			}
 
 
 		};
-		$scope.registerAccount = function() {
 
-			// 账号注册
-			if ($scope.user.accouont != undefined&&$scope.user.password!=undefined) {
-				$scope.loading = true;
-
-				// call the create function from our service (returns a promise object)
-				Todos.create($scope.formData)
-
-					// if successful creation, call our get function to get all the new todos
-					.success(function(data) {
-						$scope.loading = false;
-						$scope.formData = {}; // clear the form so our user is ready to enter another
-						$scope.todos = data; // assign our new list of todos
-					});
-			}
-
-
-		};
 
 		// DELETE ==================================================================
 		// delete a todo after checking it
