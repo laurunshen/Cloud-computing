3bc6ff6e2ed628bdb2c9d9e1cf2607879126d8a2
chase <1554658086@qq.com>
2019-05-30 17:38:28 +0800

    update

--- mean-docker/express-server/app/routes.js
+++ mean-docker/express-server/app/routes.js
@@ -56,7 +56,13 @@ module.exports = function (app) {
         });}
         else if((req.body.trans_money!=undefined)&&(req.body.trans_account!=undefined)){
             //updateBalance(req.body.account,req.body.balance);
-            var A=Todo.findOne({"account":req.body.account});
+            var A;
+            Todo.findOne({"account":req.body.account},function(err,todo){
+                if(err){
+                    res.send(err);
+                }
+                A.json(todo);
+            });
             updateBalance(A.account,20);
             getAccount(req.body.account,res);
         }
